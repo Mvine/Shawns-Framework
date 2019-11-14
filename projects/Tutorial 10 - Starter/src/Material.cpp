c0817d97153148b0507e5c062f48b71ad7005791
Shawn M <me@shawnm.ca>
2019-11-13 23:35:34 -0500

    Added tutorial 10 starter

---
+++ projects/Tutorial 10 - Starter/src/Material.cpp
@@ -0,0 +1,25 @@
+#include "Material.h"
+
+void Material::Apply() {
+	for (auto& kvp : myMat4s)
+		myShader->SetUniform(kvp.first.c_str(), kvp.second);
+	for (auto& kvp : myVec4s)
+		myShader->SetUniform(kvp.first.c_str(), kvp.second);
+	for (auto& kvp : myVec3s)
+		myShader->SetUniform(kvp.first.c_str(), kvp.second);
+	for (auto& kvp : myFloats)
+		myShader->SetUniform(kvp.first.c_str(), kvp.second);
+
+	// New in tutorial 06
+	// updated in tutorial 09
+	int slot = 0;
+	for (auto& kvp : myTextures) {
+		if (kvp.second.Sampler != nullptr)
+			kvp.second.Sampler->Bind(slot);
+		else
+			TextureSampler::Unbind(slot);
+		kvp.second.Texture->Bind(slot);
+		myShader->SetUniform(kvp.first.c_str(), slot);
+		slot++;
+	}
+}
